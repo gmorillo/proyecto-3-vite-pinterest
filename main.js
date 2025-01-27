db68ae67e6aaaf4564990bec2568b08429a03af4
Guillermo Morillo <morillo_guillermo@hotmail.com>
2025-01-27 19:33:51 +0100

    config and styles

--- main.js
+++ main.js
@@ -5,9 +5,9 @@ import { createApi } from "unsplash-js"
 let searchButton = '<svg aria-label="Icono de búsqueda" class="BNH gUZ U9O kVc" height="16" role="img" viewBox="0 0 24 24" width="16"><path d="M10 16a6 6 0 1 1 .01-12.01A6 6 0 0 1 10 16m13.12 2.88-4.26-4.26a10 10 0 1 0-4.24 4.24l4.26 4.26a3 3 0 1 0 4.24-4.24"></path></svg>'
   const headerTemplate= () => {
     return `
-    <h1><svg aria-hidden="true" aria-label="" class="g_1 gUZ U9O kVc" height="40" role="img" viewBox="0 0 24 24" width="40" fill="#e60023"><path d="M7.54 23.15q-.2-2.05.26-3.93L9 14.04a7 7 0 0 1-.35-2.07c0-1.68.81-2.88 2.09-2.88.88 0 1.53.62 1.53 1.8q0 .57-.23 1.28l-.52 1.72q-.15.5-.15.92c0 1.2.91 1.87 2.08 1.87 2.09 0 3.57-2.16 3.57-4.96 0-3.12-2.04-5.12-5.05-5.12-3.36 0-5.49 2.19-5.49 5.24 0 1.22.38 2.36 1.11 3.14-.24.41-.5.48-.88.48-1.2 0-2.34-1.69-2.34-4 0-4 3.2-7.17 7.68-7.17 4.7 0 7.66 3.29 7.66 7.33s-2.88 7.15-5.98 7.15a3.8 3.8 0 0 1-3.06-1.48l-.62 2.5a11 11 0 0 1-1.62 3.67A11.98 11.98 0 0 0 24 12a11.99 11.99 0 1 0-24 0 12 12 0 0 0 7.54 11.15"></path></svg></h1>
+    <a href="/"><svg aria-hidden="true" aria-label="" class="g_1 gUZ U9O kVc" height="40" role="img" viewBox="0 0 24 24" width="40" fill="#e60023"><path d="M7.54 23.15q-.2-2.05.26-3.93L9 14.04a7 7 0 0 1-.35-2.07c0-1.68.81-2.88 2.09-2.88.88 0 1.53.62 1.53 1.8q0 .57-.23 1.28l-.52 1.72q-.15.5-.15.92c0 1.2.91 1.87 2.08 1.87 2.09 0 3.57-2.16 3.57-4.96 0-3.12-2.04-5.12-5.05-5.12-3.36 0-5.49 2.19-5.49 5.24 0 1.22.38 2.36 1.11 3.14-.24.41-.5.48-.88.48-1.2 0-2.34-1.69-2.34-4 0-4 3.2-7.17 7.68-7.17 4.7 0 7.66 3.29 7.66 7.33s-2.88 7.15-5.98 7.15a3.8 3.8 0 0 1-3.06-1.48l-.62 2.5a11 11 0 0 1-1.62 3.67A11.98 11.98 0 0 0 24 12a11.99 11.99 0 1 0-24 0 12 12 0 0 0 7.54 11.15"></path></svg></a>
     <ul class="menu-li">
-      <li class="inicio active">Inicio</li>
+      <li class="inicio active"><a href="/">Inicio</a></li>
       <li class="explorar">Explorar</li>
       <li class="crear">Crear</li>
     </ul>
@@ -149,6 +149,7 @@ let searchButton = '<svg aria-label="Icono de búsqueda" class="BNH gUZ U9O kVc"
     btn.addEventListener("click", async () => {
       const images = await searchPhotos(input.value)
       printItems(images.response.results)
+      input.value = '';
     })
   }
 
